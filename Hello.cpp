bd0e0aa987b0ea66796e30164727b7229d0b72f1
Kirirus1 <85353849+Kirirus1@users.noreply.github.com>
2022-03-24 22:28:57 +0300

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 int main() {
-    cout << "Hello World!";
+    string name = "";
+    cout << "Enter your name";
+    cin >> name;
+    cout << "Hello, " >> name;
     return 0;
 }
